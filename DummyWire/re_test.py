0cf0f059ef61882a80f70f0e8569af3290bd91ca
xiangyu <xiangyuzhang365@gmail.com>
2016-02-08 15:46:17 -0500

    delete trivial documents

--- DummyWire/re_test.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'xiangyuzhang'
-import re
-
-unexpected EOF while parsing (<string>, line 1)
-unexpected EOF while parsing (<string>, line 1)
--- Obfusgate/test.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'xiangyuzhang'
-import re
-string = "and3 gate11( .a(N29), .b(N75), .c(N42), .O(N290) );"
-
-# print re.sub((N29), "(N29_OBF)", string)
-print string.replace("(N29)", "(N29_OBF)")
